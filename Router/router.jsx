53d7dc3c85fcad38600af5b4e1d80d2c7b7de256
Your Name <you@example.com>
2025-11-28 18:27:06 +0100

    ajouter fichier

--- Router/router.jsx
+++ Router/router.jsx
@@ -1,55 +1,41 @@
-
-import Home  from '../pages/Home'
-import Login from '../pages/Login'
-import Signup from '../pages/Sign'
-import  NoteFound  from '../pages/notefound'
-import Layaout from '../layaout/layaout'
-import Dashboard from '../DashBoard/dashboard'
- import {  createHashRouter } from 'react-router-dom';
-//import {  createBrowserRouter } from 'react-router-dom'
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Sign";
+import NoteFound from "../pages/notefound";
+import Layaout from "../layaout/layaout";
+import Dashboard from "../DashBoard/dashboard";
+import { createHashRouter } from 'react-router-dom';
+//import { createBrowserRouter } from "react-router-dom";
 
 // import UserList from '../src/components/Users/UserList'
 // import StudentsPage from '../DashBoard/components/Students'
 
-export const  router = createHashRouter([
+export const router =createHashRouter ([
   {
-   element: <Layaout />,
-    children : [
- {
-        path: '/',
-        element:<Home />
-    },
-    
-     {
-        path: '/dashboard',
-       element : <Dashboard />
-    },
-  {
-    path: '/login',
-    element:<Login />
+    element: <Layaout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+
+      {
+        path: "*",
+        element: <NoteFound />,
+      },
+    ],
   },
-   {
-    path: '/signup',
-    element: <Signup />
-  },
-  //  {
-  //   path: '/usr',
-  //   element: <UserList />
-  // },
-    {
-    path: '*',
-    element: <NoteFound />
-  }
-    ]
-
-  },
-   
-
-])
-
-
-
-
-
-
-
+]);
