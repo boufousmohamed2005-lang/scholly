36e2f94c052b3e901c3b02229465370d49ccd676
Your Name <you@example.com>
2025-11-28 18:51:00 +0100

    router

--- Router/router.jsx
+++ Router/router.jsx
@@ -1,60 +1,24 @@
-// import Home from "../pages/Home";
-// import Login from "../pages/Login";
-// import Signup from "../pages/Sign";
-// import NoteFound from "../pages/notefound";
-// import Layaout from "../layaout/layaout";
-// import Dashboard from "../DashBoard/dashboard";
-// import {  createHashRouter } from 'react-router-dom';
-// //import { createBrowserRouter } from "react-router-dom";
-
-// // import UserList from '../src/components/Users/UserList'
-// // import StudentsPage from '../DashBoard/components/Students'
-
-// export const router = createHashRouter([
-//   {
-//     element: <Layaout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home />,
-//       },
-
-//       {
-//         path: "/dashboard",
-//         element: <Dashboard />,
-//       },
-//       {
-//         path: "/login",
-//         element: <Login />,
-//       },
-//       {
-//         path: "/signup",
-//         element: <Signup />,
-//       },
-
-//       {
-//         path: "*",
-//         element: <NoteFound />,
-//       },
-//     ],
-//   },
-// ]);
-
- import Home from "../pages/Home";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Sign";
 import NoteFound from "../pages/notefound";
 import Layaout from "../layaout/layaout";
 import Dashboard from "../DashBoard/dashboard";
 import {  createHashRouter } from 'react-router-dom';
+//import { createBrowserRouter } from "react-router-dom";
+
+// import UserList from '../src/components/Users/UserList'
+// import StudentsPage from '../DashBoard/components/Students'
+
 export const router = createHashRouter([
   {
-    element: <Layout />,
+    element: <Layaout />,
     children: [
       {
         path: "/",
         element: <Home />,
       },
+
       {
         path: "/dashboard",
         element: <Dashboard />,
@@ -67,9 +31,10 @@ export const router = createHashRouter([
         path: "/signup",
         element: <Signup />,
       },
+
       {
         path: "*",
-        element: <NotFound />,
+        element: <NoteFound />,
       },
     ],
   },
