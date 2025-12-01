e24035297269244e394f68476cf332c09acaa2d4
Your Name <you@example.com>
2025-12-01 11:56:43 +0100

    fix remote

--- Router/router.jsx
+++ Router/router.jsx
@@ -5,12 +5,12 @@ import NoteFound from "../pages/notefound";
 import Layaout from "../layaout/layaout";
 import Dashboard from "../DashBoard/dashboard";
 import {  createHashRouter } from 'react-router-dom';
-  //import { createBrowserRouter } from "react-router-dom";
+//import { createBrowserRouter } from "react-router-dom";
 
 // import UserList from '../src/components/Users/UserList'
 // import StudentsPage from '../DashBoard/components/Students'
 
-export const router = createHashRouter ([
+export const router =createHashRouter  ([
   {
     element: <Layaout />,
     children: [
