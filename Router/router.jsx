c84a0286c78f9f297fadcfbd1c89c0a3e5b24125
Your Name <you@example.com>
2025-12-02 17:58:25 +0100

    api

--- Router/router.jsx
+++ Router/router.jsx
@@ -4,8 +4,8 @@ import Signup from "../pages/Sign";
 import NoteFound from "../pages/notefound";
 import Layaout from "../layaout/layaout";
 import Dashboard from "../DashBoard/dashboard";
-//import {  createHashRouter } from 'react-router-dom';
-import { createBrowserRouter } from "react-router-dom";
+import {  createHashRouter } from 'react-router-dom';
+//import { createBrowserRouter } from "react-router-dom";
 
 // import UserList from '../src/components/Users/UserList'
 // import StudentsPage from '../DashBoard/components/Students'
